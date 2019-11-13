11e2cda4e0ad55642f05d45e25b15f2aac26c3a6
MC-Atom <40128144+MC-Atom@users.noreply.github.com>
2019-11-13 08:40:21 -0800

    testing files

--- src/schedule/index.ts
+++ src/schedule/index.ts
@@ -11,6 +11,23 @@ import { NoSchoolSchedule, NoEventSchedule, WeirdAssemblySchedule, WeirdAssembly
         PreFinals1345Schedule, FinalAssemblySchedule78, FinalAssemblySchedule12, Finals34Schedule, Finals15Schedule,
         Finals26Schedule, FinalsTBDSchedule, SummerSchoolSchedule, HSBackToSchoolNight } from './schedules';
 
+// Requiring fs module in which 
+// readFile function is defined. 
+const fs = require('fs') 
+  
+// Reading data in utf-8 format 
+// which is a type of character set. 
+// Instead of 'utf-8' it can be  
+// other character set also like 'ascii' 
+fs.readFile('Input.txt', 'utf-8', (err, data) => { 
+    if (err) throw err; 
+  
+    // Converting Raw Buffer to text 
+    // data using tostring function. 
+    console.log(data); 
+}) 
+
+
 export const plusDays = 0;
 
 export const allGrades = [7, 8, 9, 10, 11, 12, 13];
